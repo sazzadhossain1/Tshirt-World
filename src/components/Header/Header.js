e523f61c83972cbd436f54a547ae80496a55957e
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-04-02 01:17:36 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Welcome to Tshirt mania!!!</h2>
+            <nav>
+                
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
